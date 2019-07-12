eb4df08b268cf881937ff178f5dec223fec9730c
Ingmar van Dijk <ingmar.van.dijk@philips.com>
2019-07-12 15:32:02 +0200

    Ignoring lines starting with multiline comment start sequence

--- src/com/philips/kata/CodeCounter.java
+++ src/com/philips/kata/CodeCounter.java
@@ -6,9 +6,14 @@ public class CodeCounter {
     public long countLines(String code) {
         if (code.isEmpty()) {
             return 0;
-        } else {
-             String[] lines = code.split("\\n");
-             return Arrays.stream(lines).map(String::trim).filter(l -> !l.isEmpty()).filter(l -> !l.startsWith("//")).count();
         }
+
+        String[] lines = code.split("\\n");
+        return Arrays.stream(lines)
+                .map(String::trim)
+                .filter(l -> !l.isEmpty())
+                .filter(l -> !l.startsWith("//"))
+                .filter(l -> !l.startsWith("/*"))
+                .count();
     }
 }

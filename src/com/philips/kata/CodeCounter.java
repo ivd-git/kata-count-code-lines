18746de49c8a8e5c341cabce7a500465d7f82520
Ingmar van Dijk <ingmar.van.dijk@philips.com>
2019-07-13 14:18:14 +0200

    Implementation of ignoring lines of code when in multiline comment

--- src/com/philips/kata/CodeCounter.java
+++ src/com/philips/kata/CodeCounter.java
@@ -1,15 +1,29 @@
 package com.philips.kata;
 
 import java.util.Arrays;
+import java.util.function.Predicate;
 
 public class CodeCounter {
+
+    private boolean insideMultiLineComment;
+
     public long countLines(String code) {
         String[] lines = code.split("\\n");
         return Arrays.stream(lines)
                 .map(String::trim)
                 .filter(l -> !l.isEmpty())
                 .filter(l -> !l.startsWith("//"))
-                .filter(l -> !l.startsWith("/*"))
+                .filter(multiLineComments())
                 .count();
+    }
+
+    private Predicate<String> multiLineComments() {
+
+        return l -> {
+            if (!insideMultiLineComment) {
+                insideMultiLineComment = l.startsWith("/*");
+            }
+            return !insideMultiLineComment;
+        };
     }
 }
